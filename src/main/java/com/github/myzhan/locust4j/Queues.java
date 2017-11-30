768bc6396adf47837c2d07a4fc278639bc787f3a
myzhan <scau.myzhan@gmail.com>
2017-11-30 13:49:57 +0800

    add docs of Queues class

--- src/main/java/com/github/myzhan/locust4j/Queues.java
+++ src/main/java/com/github/myzhan/locust4j/Queues.java
@@ -5,8 +5,16 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
+/**
+ * Every components of locust4j use queues to communicate to each other.
+ */
 public class Queues {
 
+    /**
+     * User code reports successful and failed records to Stats, by adding message to these queue.
+     * If the sending speed is too fast, single-threaded stats may be a bottleneck and reports wrong RPS.
+     * And, since these two queues are unbounded, the JVM may run out of memory.
+     */
     protected static final ConcurrentLinkedQueue<RequestSuccess> REPORT_SUCCESS_TO_STATS
         = new ConcurrentLinkedQueue<RequestSuccess>();
     protected static final ConcurrentLinkedQueue<RequestFailure> REPORT_FAILURE_TO_STATS
