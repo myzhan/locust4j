5824c06ec58fbae1f7d3115d0d4e052ac127681a
myzhan <scau.myzhan@gmail.com>
2018-12-06 10:27:05 +0800

    add tests for rpc

---
+++ src/test/java/com/github/myzhan/locust4j/rpc/TestServer.java
@@ -0,0 +1,63 @@
+package com.github.myzhan.locust4j.rpc;
+
+import java.io.IOException;
+
+import com.github.myzhan.locust4j.message.Message;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
+
+/**
+ * @author myzhan
+ * @date 2018/12/05
+ */
+public class TestServer {
+
+    private ZContext context;
+    private String bindHost;
+    private int pushPort;
+    private int pullPort;
+    private ZMQ.Socket pushSocket;
+    private ZMQ.Socket pullSocket;
+    private Thread serverThread;
+
+    public TestServer(String bindHost, int pushPort, int pullPort) {
+        this.context = new ZContext();
+        this.bindHost = bindHost;
+        this.pushPort = pushPort;
+        this.pullPort = pullPort;
+    }
+
+    public void start() {
+        pushSocket = context.createSocket(ZMQ.PUSH);
+        pushSocket.bind(String.format("tcp://%s:%d", bindHost, pushPort));
+        pullSocket = context.createSocket(ZMQ.PULL);
+        pullSocket.bind(String.format("tcp://%s:%d", bindHost, pullPort));
+
+        serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(true) {
+                        byte[] packet = pullSocket.recv();
+                        Message message = new Message(packet);
+                        pushSocket.send(message.getBytes());
+                    }
+                } catch (ZMQException ex) {
+                    // ignore ZMQException, it may be interrupted.
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        serverThread.start();
+    }
+
+    public void stop() {
+        serverThread.interrupt();
+        pushSocket.close();
+        pullSocket.close();
+        context.close();
+    }
+}
---
+++ src/test/java/com/github/myzhan/locust4j/rpc/TestZeromqClient.java
@@ -0,0 +1,36 @@
+package com.github.myzhan.locust4j.rpc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.myzhan.locust4j.message.Message;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author myzhan
+ * @date 2018/12/05
+ */
+public class TestZeromqClient {
+
+    @Test
+    public void TestPingPong() throws Exception {
+        TestServer server = new TestServer("127.0.0.1", 15557+1, 15557);
+        server.start();
+
+        Client client = new ZeromqClient("127.0.0.1", 15557);
+        Map data = new HashMap();
+        data.put("hello", "world");
+
+        client.send(new Message("test", data, "node"));
+        Message message = client.recv();
+
+        Assert.assertEquals("test", message.getType());
+        Assert.assertEquals("node", message.getNodeID());
+        Assert.assertEquals(data, message.getData());
+
+        Thread.sleep(100);
+        server.stop();
+        client.close();
+    }
+}
