538b07bc5d8020a60383a36ee5fd648eb34ec394
myzhan <scau.myzhan@gmail.com>
2018-12-11 11:22:19 +0800

    meet JMH

---
+++ src/benchmark/java/com/github/myzhan/locust4j/utils/BenchmarkMD5.java
@@ -0,0 +1,30 @@
+package com.github.myzhan.locust4j.utils;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * @author myzhan
+ * @date 2018/12/11
+ */
+public class BenchmarkMD5 {
+
+    @Benchmark
+    public String calculateMD5() {
+        return Utils.md5("hello", "world", "locust4j");
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+            .include(BenchmarkMD5.class.getSimpleName())
+            .forks(1)
+            .warmupIterations(1)
+            .measurementIterations(2)
+            .build();
+
+        new Runner(opt).run();
+    }
+}
