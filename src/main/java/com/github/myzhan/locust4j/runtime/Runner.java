628d118192c98520db95a34cd9d0a9aa40e13b1a
myzhan <scau.myzhan@gmail.com>
2019-01-09 19:21:38 +0800

    FIX: missing param

--- src/main/java/com/github/myzhan/locust4j/runtime/Runner.java
+++ src/main/java/com/github/myzhan/locust4j/runtime/Runner.java
@@ -215,7 +215,7 @@ public class Runner {
 
         if (!"hatch".equals(type) && !"stop".equals(type) && !"quit".equals(type)) {
             Log.error(String
-                .format("Got %s message from master, which is not supported, please report an issue to locust4j."));
+                .format("Got %s message from master, which is not supported, please report an issue to locust4j.", type));
             return;
         }
 
