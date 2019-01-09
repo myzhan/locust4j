cc3d81416dbf6a5a9aec5438078f1ee667aa176c
myzhan <scau.myzhan@gmail.com>
2019-01-09 18:07:32 +0800

    add an example for RampUpRateLimiter

---
+++ examples/ratelimit/RampUpRps.java
@@ -0,0 +1,49 @@
+package ratelimit;
+
+import com.github.myzhan.locust4j.AbstractTask;
+import com.github.myzhan.locust4j.Locust;
+import com.github.myzhan.locust4j.ratelimit.AbstractRateLimiter;
+import com.github.myzhan.locust4j.ratelimit.RampUpRateLimiter;
+
+import java.util.concurrent.TimeUnit;
+
+public class RampUpRps {
+
+    private static class TestTask extends AbstractTask {
+        public int weight;
+        public String name;
+
+        public TestTask(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        @Override
+        public int getWeight() {
+            return this.weight;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public void execute() {
+            Locust.getInstance().recordSuccess("test", name, 1, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Locust locust = Locust.getInstance();
+        locust.setVerbose(true);
+        locust.setMaxRPS(1000);
+
+        // use a custom rate limiter
+        AbstractRateLimiter rateLimiter = new RampUpRateLimiter(100, 10, 1, TimeUnit.SECONDS,
+                1, TimeUnit.SECONDS);
+        locust.setRateLimiter(rateLimiter);
+
+        locust.run(new TestTask("foo", 10));
+    }
+}
