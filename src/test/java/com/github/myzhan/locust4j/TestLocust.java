85a59c486ed59eb4d31caf683ba61101e10f100b
sunnychanwork <sunny.chan@block.one>
2020-12-01 11:34:37 +0800

    Adding onStart and onStop method

--- src/test/java/com/github/myzhan/locust4j/TestLocust.java
+++ src/test/java/com/github/myzhan/locust4j/TestLocust.java
@@ -2,6 +2,8 @@ package com.github.myzhan.locust4j;
 
 import com.github.myzhan.locust4j.ratelimit.AbstractRateLimiter;
 import com.github.myzhan.locust4j.ratelimit.StableRateLimiter;
+import com.github.myzhan.locust4j.runtime.Runner;
+import com.github.myzhan.locust4j.runtime.RunnerState;
 import com.github.myzhan.locust4j.stats.RequestFailure;
 import com.github.myzhan.locust4j.stats.RequestSuccess;
 import com.github.myzhan.locust4j.stats.Stats;
@@ -21,6 +23,7 @@ public class TestLocust {
 
         private int weight;
         private final String name;
+        private boolean onStartCalled = false, onStopCalled = false; 
 
         public TestTask(int weight, String name) {
             this.weight = weight;
@@ -38,8 +41,18 @@ public class TestLocust {
         }
 
         @Override
-        public void execute() {
+        public void execute() throws Exception {
             weight++;
+        }
+        
+        @Override
+        public void onStart() throws Exception {
+            onStartCalled=true;
+        }
+
+        @Override
+        public void onStop() {
+            onStopCalled=true;
         }
 
     }
@@ -66,7 +79,55 @@ public class TestLocust {
         tasks.add(task);
 
         Locust.getInstance().dryRun(tasks);
-        assertEquals(3, task.getWeight());
+        
+        assertEquals(3, task.getWeight());        
+    }    
+
+    class TestRunner extends Runner {
+        RunnerState state;
+        public void setState(RunnerState state) {
+            this.state = state;
+        }
+        @Override
+        public RunnerState getState() {          
+            return state;
+        }
+    }
+
+    @Test
+    public void TestRunnerOnStartAndOnStop() {
+        TestRunner runner = new TestRunner();
+        Locust.getInstance().setRunner(runner);
+        TestTask task = new TestTask(1, "test");
+        runner.setState(RunnerState.Stopped);
+        task.run();
+        assertTrue(task.onStartCalled);
+        assertTrue(task.onStopCalled);
+    }
+
+    @Test
+    public void TestOnStartAndOnStop() {
+        TestTask task = new TestTask(1, "test");
+        Locust.getInstance().dryRun(task);
+
+        assertTrue("onStart must be called", task.onStartCalled);
+        assertTrue("onStop must be called", task.onStopCalled);        
+
+        ArrayList<AbstractTask> tasks = new ArrayList<>();
+        tasks.add(task);
+
+        Locust.getInstance().dryRun(tasks);
+        assertTrue("onStart must be called", task.onStartCalled);
+        assertTrue("onStop must be called", task.onStopCalled);        
+        task = new TestTask(0, "test") {
+            @Override
+            public void execute() throws Exception {            
+              throw new Exception("Test");
+            }
+        };
+        Locust.getInstance().dryRun(task);
+        assertTrue("onStart must be called", task.onStartCalled);
+        assertTrue("onStop must be called", task.onStopCalled);
     }
 
     @Test
