49dfceb8e502eff4eca2273c1943f1d125d46477
sergii-volokh <30294278+sergii-volokh@users.noreply.github.com>
2025-02-13 19:48:12 +0200

    Add synchronization on zmq messages send

--- src/main/java/com/github/myzhan/locust4j/rpc/ZeromqClient.java
+++ src/main/java/com/github/myzhan/locust4j/rpc/ZeromqClient.java
@@ -1,10 +1,12 @@
 package com.github.myzhan.locust4j.rpc;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 
 import com.github.myzhan.locust4j.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQException;
 
@@ -23,40 +25,53 @@ public class ZeromqClient implements Client {
     private final ZMQ.Context context = ZMQ.context(1);
     private final String identity;
     private final ZMQ.Socket dealerSocket;
+    private final Object dealerSocketSync = new Object();
 
     public ZeromqClient(String host, int port, String nodeID) {
         this.identity = nodeID;
-        this.dealerSocket = context.socket(ZMQ.DEALER);
+        this.dealerSocket = context.socket(SocketType.DEALER);
         this.dealerSocket.setIdentity(this.identity.getBytes());
+        this.dealerSocket.setImmediate(true);
+        this.dealerSocket.setReceiveTimeOut(300);
         boolean connected = this.dealerSocket.connect(String.format("tcp://%s:%d", host, port));
         if (connected) {
             logger.debug("Locust4j is connected to master({}:{})", host, port);
         } else {
             logger.debug("Locust4j isn't connected to master({}:{}), please check your network situation", host, port);
         }
-
     }
 
     @Override
     public Message recv() throws IOException {
-        try {
-            byte[] bytes = this.dealerSocket.recv();
-            return new Message(bytes);
-        } catch (ZMQException ex) {
-            throw new IOException("Failed to receive ZeroMQ message", ex);
+        while (true) {
+            try {
+                byte[] bytes = this.dealerSocket.recv();
+                if (bytes != null) return new Message(bytes);
+            } catch (ZMQException ex) {
+                throw new IOException("Failed to receive ZeroMQ message", ex);
+            }
+            if (Thread.currentThread().isInterrupted())
+                throw new InterruptedIOException();
         }
     }
 
     @Override
     public void send(Message message) throws IOException {
-        byte[] bytes = message.getBytes();
-        this.dealerSocket.send(bytes);
+        try {
+            byte[] bytes = message.getBytes();
+            synchronized(dealerSocketSync) {
+                dealerSocket.send(bytes);
+            }
+        } catch(Exception ex) {
+            throw new IOException("Failed to send message to master", ex);
+        }
     }
 
     @Override
     public void close() {
-        dealerSocket.close();
+        synchronized(dealerSocketSync) {
+            dealerSocket.close();
+        }
         context.close();
     }
 }
-
